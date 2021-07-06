35c4348c708127586605fa751c5b6c0682b0e65c
youhancheery <youhancheery@gmail.com>
2021-07-06 15:54:01 +1000

    Fixed init points

--- studies_comparison.R
+++ studies_comparison.R
@@ -1,8 +1,6 @@
 # measurement on the three datasets Pavel's contrastive divergence paper
 # states are challenging for a network to estimate
 
-m <- modules::use("~/Documents/math5005/msresearch/utils.R") 
-
 # m$reinstall_ergm()
 library(ergm)
 library(ergm.count)
@@ -11,37 +9,27 @@ set.seed(1234)
 
 # load all the different datasets
 load("~/Documents/math5005/msresearch/data/supp_datasets.RData")
+m <- modules::use("~/math5005/msresearch/utils.R") 
 
-# create a function that reads any single dataset
-# and builds a model off the back of it
-# need to time the time taken to reach the result
+# ecoli
+output_ecoli2 <- m$time_response(data=ecoli2,
+                                 ecoli2 ~ edges + degree(2:5) + gwdegree(0.25, fixed = TRUE))
+
+output_ecoli2$`Stochastic-Approximation:zeros`$coef
+output_ecoli2$`Stochastic-Approximation:NULL`$coef
+
+
+ouput_ecoli2_self <- m$time_response(data=ecoli2,
+                                     ecoli2 ~ edges + degree(2:5) + gwdegree(0.25, fixed = TRUE) + nodemix("self", base = 1))
+
 
 # time response for each model (3) across each dataset (3) for a total of 9 fits
 # kapferer
-kapferer_study <- m$time_response(data=kapferer,
+study_kapferer <- m$time_response(data=kapferer,
                                   formula=kapferer~edges+gwdegree(0.25,fixed=TRUE)+gwesp(0.25, fixed=TRUE)+gwdsp(0.25,fixed=TRUE))
-# ecoli
-ecoli2_study <- m$time_response(data=ecoli2,
-                                ecoli2 ~ edges + degree(2:5) + gwdegree(0.25, fixed = TRUE))
-ecoli2_self_study <- m$time_response(data=ecoli2,
-                                     ecoli2 ~ edges + degree(2:5) + gwdegree(0.25, fixed = TRUE) + nodemix("self", base = 1))
 # zach
 # TODO: not sure why this isn't working? 
 # nonzero deprecated in current version of ergm?
 zach_study <- m$time_response(data = zach,
                               formula = zach ~ nonzero+sum+nodefactor("role",base=2)+transitiveweights)
-
-
-
-run.study("kap.study", kapferer ~ edges + gwdegree(0.25, fixed = TRUE) + gwesp(0.25, fixed = TRUE) + gwdsp(0.25, fixed = TRUE))
-run.study("zach.binom.study", zach ~ nonzero+sum+nodefactor("role",base=2)+transitiveweights, reference = ~Binomial(8), response = "contexts", ctrl.cd=control.ergm(CD.maxit=60,CD.trustregion=100000,MCMLE.trustregion=100000))
-run.study("zach.cmp.study", zach ~ nonzero+CMP+sum+nodefactor("role",base=2)+transitiveweights, reference  = ~Poisson, response = "contexts", ctrl.cd=control.ergm(CD.maxit=60,CD.trustregion=100000,MCMLE.trustregion=100000))
-run.study("kap2.study", kapferer ~ edges + gwesp(0.25, fixed = TRUE) + gwdsp(0.25, fixed = TRUE))
-run.study("ecoli2.study", ecoli2 ~ edges + degree(2:5) + gwdegree(0.25, fixed = TRUE))
-run.study("ecoli2.self.study", ecoli2 ~ edges + degree(2:5) + gwdegree(0.25, fixed = TRUE) + nodemix("self", base = 1))
-run.study("laz2.c.study", laz ~ gwesp()+nodecov("seniority")+nodefactor("practice")+nodematch("practice")+nodematch("gender")+nodematch("office"), constraints = ~edges)
-run.study("laz2.study", laz ~ edges+gwesp()+nodecov("seniority")+nodefactor("practice")+nodematch("practice")+nodematch("gender")+nodematch("office"))
-
-
-
 
--- utils.R
+++ utils.R
@@ -6,155 +6,79 @@ reinstall_ergm <- function() {
   devtools::install_github("youhancheery/ergm")
 }
 
+# estimate_parameters <- function {}
 
-time_response <- function(data, formula, init=NULL, init.method="zeros") {
+time_response <- function(data, formula) {
 
-  methods <- c("Stochastic-Approximation", "EE")
+  # different methods we will be running through
   # TODO: understand why robbins-monro isn't running properly
+  methods <- c("Stochastic-Approximation", "EE")
+  
   final <- list()
+  
+  # initialisation methods are
+  # 1. vector of zeros
+  # 2, MPLE
+  # 3. vector of zeros except edges density parameters (TODO)
+  
+  init_methods <- c("zeros", "MPLE")
   for(method in methods) {
-    for(init in inits) {
-      print(paste("Running estimation using", method))
-      control = ergm::control.ergm(main.method = method, # estimation method
-                                   init=init, # initial values
-                                   init.method=init.method, # 
-                                   MCMC.runtime.traceplot = FALSE,
-                                   MCMC.return.stats = TRUE)
+    for(init_method in init_methods) {
+      print(paste("Running estimation using", method, "starting at", init_method))
+      if(init_method == "zeros") {
+        # starting point initialised at all zeros
+        # TODO need to add MCMC.samplesize to be high enough number
+        control = ergm::control.ergm(main.method = method, # estimation method
+                                     init=NULL, # always null so init.method passes values
+                                     init.method=init_method, # 
+                                     MCMC.runtime.traceplot = FALSE,
+                                     MCMC.return.stats = TRUE,
+                                     MCMLE.steplength = .25,
+                                     MCMC.burnin = 100000, 
+                                     MCMC.interval = 50000, 
+                                     MCMC.samplesize = 2500,
+                                     MCMLE.maxit = 100)
+        runtime <- system.time(output <- ergm::ergm(formula=formula, 
+                                                    control=control,
+                                                    eval.loglik = FALSE))
+        # coef <- stats::coef(output)
+        coef <- output$coef
+        iter <- output$iterations
+        is_fail <- output$failure
+        init <- output$coef.init
+      } else {
+        # using bernoulli reference we have initial starting point at MPLE
+        control = ergm::control.ergm(main.method = method, # estimation method
+                                     init=NULL, # initial values
+                                     init.method=init_method, # 
+                                     MCMC.runtime.traceplot = FALSE,
+                                     MCMC.return.stats = TRUE,
+                                     MCMLE.steplength = .25,
+                                     MCMC.burnin = 100000, 
+                                     MCMC.interval = 50000, 
+                                     MCMC.samplesize = 2500,
+                                     MCMLE.maxit = 100)
+        runtime <- system.time(output <- ergm::ergm(formula=formula,
+                                                    control=control,
+                                                    reference=~Bernoulli,
+                                                    eval.loglik = FALSE))
+        # coef <- stats::coef(output)
+        coef <- output$coef
+        iter <- output$iterations
+        is_fail <- output$failure
+        init <- output$coef.init
+      }
       
-      runtime <- system.time(output <- ergm::ergm(formula=formula, control=control, eval.loglik = FALSE))
-      coef <- stats::coef(output)
-      iter <- output$iterations
-      final[method] <- list(list("initialisation"=init,
-                                 "timings"=runtime,
-                                 "coef"=coef,
-                                 "iterations"=iter)) 
+      final[paste0(method, ":", init_method)] <- list(list("init"=init,
+                                                           "timings"=runtime,
+                                                           "coef"=coef,
+                                                           "iterations"=iter,
+                                                           "is_fail"=is_fail)) 
     }
   }
   return(final)
 }
 
-run.study <- function(name, formula, response=NULL, reference=~Bernoulli, constraints=~., 
-                      ctrl.cd=control.ergm(CD.maxit=60,CD.trustregion=100000, CD.Hummel.miss.sample=10),
-                      ctrl.simulate=control.simulate.formula(), nsim=1024, 
-                      k.levels=K.LEVELS, m.levels=M.LEVELS, km.max=KM.MAX, 
-                      cd.metrics=CD.METRICS, steplen.margins=STEPLEN.MARGINS, 
-                      reps=REPS, mple=MPLE, intercept=INTERCEPT, rm.confs=RM.CONFS){
+mple_base <- function() {
   
-  ctrl.cd$MCMC.samplesize <- 1024
-  ctrl.cd$parallel <- 0
-  
-  lapply(seq_len(reps), function(rep){
-    # Start the intercept algorithm.
-    if(intercept){
-      limitmyps(PSLIMIT)
-      mcparallel({study.fit(name, formula, response=response, reference=reference, constraints=constraints, estimate="intercept", ctrl.cd=ctrl.cd, ctrl.simulate=ctrl.simulate, nsim=nsim, seed=rep)},detached=TRUE)
-    }
-    # Start the MPLE.
-    if(mple){
-      limitmyps(PSLIMIT)
-      mcparallel({study.fit(name, formula, response=response, reference=reference, constraints=constraints, estimate="MPLE", ctrl.cd=ctrl.cd, ctrl.simulate=ctrl.simulate, nsim=nsim, seed=rep)},detached=TRUE)
-    }
-    
-    # Start the CD RM with the specified parameters.
-    for(rm.conf in rm.confs){
-      for(k in k.levels){
-        for(m in m.levels){
-          if(k*m>km.max) next
-          ctrl.cdkm <- ctrl.cd
-          ctrl.cdkm$CD.method <- "Robbins-Monro"
-          ctrl.cdkm$CD.RM.a0 <- rm.conf$a0
-          ctrl.cdkm$CD.RM.c <- rm.conf$c
-          ctrl.cdkm$CD.nsteps <- k
-          ctrl.cdkm$CD.multiplicity <- m
-          ctrl.cdkm$CD.nsteps.obs <- k
-          ctrl.cdkm$CD.multiplicity.obs <- m
-          limitmyps(PSLIMIT)
-          mcparallel({study.fit(name, formula, response=response, reference=reference, constraints=constraints, estimate="CD", ctrl.cd=ctrl.cdkm, ctrl.simulate=ctrl.simulate, nsim=nsim, seed=rep)}, detached=TRUE)
-        }
-      }
-    }
-    
-    
-    # Start the CD MCMLE with the specified parameters.
-    for(metric in cd.metrics){
-      for(steplen.margin in steplen.margins){
-        for(k in k.levels){
-          for(m in m.levels){
-            if(k*m>km.max) next
-            ctrl.cdkm <- ctrl.cd
-            ctrl.cdkm$CD.metric <- metric
-            ctrl.cdkm$CD.steplength.margin <- steplen.margin
-            ctrl.cdkm$CD.nsteps <- k
-            ctrl.cdkm$CD.multiplicity <- m
-            ctrl.cdkm$CD.nsteps.obs <- k
-            ctrl.cdkm$CD.multiplicity.obs <- m
-            limitmyps(PSLIMIT)
-            mcparallel({study.fit(name, formula, response=response, reference=reference, constraints=constraints, estimate="CD", ctrl.cd=ctrl.cdkm, ctrl.simulate=ctrl.simulate, nsim=nsim, seed=rep)}, detached=TRUE)
-          }
-        }
-      }
-    }
-  })
 }
-
-# Fit an ERGM with specified parameters, perform a simulation to
-# obtain the measures of the quality of starting values.
-study.fit <- function(name, formula, response, reference, constraints, estimate, ctrl.cd, ctrl.simulate, nsim, seed){
-  if(estimate=="intercept"){
-    nw <- ergm.getnetwork(formula)
-    m<-ergm.getmodel(formula, nw, response=response,role="target")
-    cn <- unlist(lapply(m$terms, function(t) NVL(names(t$params), t$coef.names)))
-    o <- offset.info.formula(formula, response=response)
-    epos <- which(cn=="edges" & !o$theta)
-    
-    theta0 <- rep(0,length(cn))
-    if(is.null(response) && constraints!=~. && length(epos)){
-      d <- network.edgecount(nw, na.omit=TRUE)/network.dyadcount(nw, na.omit=TRUE)
-      epar <- log(d/(1-d))
-      theta0[epos] <- epar    
-    }else theta0 <- NULL
-    
-    fit <- list(coef=theta0, est="intercept")
-  }else{
-    fit <- time.ergm(formula, response=response, reference=reference, constraints=constraints, estimate=estimate, control=ctrl.cd, verbose=FALSE, seed=seed)
-    fit <- list(est=fit$est, timings=fit$timings, coef=fit$coef, k=fit$ctrl$CD.nsteps, m=fit$ctrl$CD.multiplicity, metric=fit$ctrl$CD.metric, steplen.margin=fit$ctrl$CD.steplength.margin, iterations=fit$iterations,seed=fit$seed, RM.a0=fit$ctrl$CD.RM.a0, RM.c=fit$ctrl$CD.RM.c, method=fit$ctrl$CD.method)
-  }
-  
-  if(!is.null(fit$coef)){
-    gs <- summary(formula, response=response)
-    m <- ergm.getmodel(formula, ergm.getnetwork(formula), response=response, role="target")
-    obs.stats <- ergm:::.ergm.esteq(fit$coef, m, rbind(gs))
-    
-    cat("===== Simulating full =====\n")
-    print(fit)
-    set.seed(fit$seed)
-    sim.stats.long <- simulate(formula, response=response, reference=reference, coef=fit$coef, constraints=constraints, esteq=TRUE, control=ctrl.simulate, nsim=nsim, statsonly=TRUE)
-    cat("===== End simulating full =====\n")
-    
-    cat("===== Simulating short =====\n")
-    print(fit)
-    set.seed(fit$seed)
-    sim.stats.short <- simulate(formula, response=response, reference=reference, coef=fit$coef, constraints=constraints, esteq=TRUE, control=modifyList(ctrl.simulate, list(MCMC.burnin=nsim*8, MCMC.interval=8)), nsim=nsim, statsonly=TRUE)
-    cat("===== End simulating short =====\n")
-    
-    cat("===== Fitting ERGM: IS =====\n")
-    fit.IS <- time.ergm(formula, response=response, reference=reference, constraints=constraints, estimate="MLE",
-                        control=modifyList(ctrl.cd, list(init=fit$coef, MCMLE.metric="naive")), verbose=FALSE, seed=seed)
-    cat("===== End fitting ERGM: IS =====\n")
-    
-    cat("===== Fitting ERGM: Lognormal =====\n")
-    fit.LN <- time.ergm(formula, response=response, reference=reference, constraints=constraints, estimate="MLE",
-                        control=modifyList(ctrl.cd, list(init=fit$coef, MCMLE.metric="lognormal")), verbose=FALSE, seed=seed)
-    cat("===== End fitting ERGM: Lognormal =====\n")
-    
-    
-    # Store the fit information.
-    fit <- c(fit, list(Hummel1Step=ERRVL(try(ergm:::.Hummel.steplength(sim.stats.long, obs.stats)), 0),
-                       Hummel1Step.short=ERRVL(try(ergm:::.Hummel.steplength(sim.stats.short, obs.stats)), 0),
-                       MLE.IS=fit.IS,
-                       MLE.LN=fit.LN))
-  }
-  
-  save(fit, file=paste(name, Sys.time(), Sys.getpid(), round(runif(1)*1e10), "RData", sep="."))
-}
-
